3d195459c862dfacb2009ecec768393f788aa3ee
Bartlomiej <szybiakbartlomiej@gmail.com>
2023-02-20 13:24:01 +0100

    Uddate strategy model in User adn create repository who extends JPA Repository

--- src/main/java/com/example/apitask/model/User.java
+++ src/main/java/com/example/apitask/model/User.java
@@ -14,7 +14,7 @@ import lombok.NonNull;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "firstname", nullable = false)
