cb613f54e969a78eb17a2b76160cb5318b96158d
Bartlomiej <szybiakbartlomiej@gmail.com>
2023-02-20 17:19:08 +0100

    Add method for Get user by Last Name

--- src/main/java/com/example/apitask/controller/UserController.java
+++ src/main/java/com/example/apitask/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.apitask.controller;
 import com.example.apitask.exception.ElementAlredyExistsException;
 import com.example.apitask.model.User;
 import com.example.apitask.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -12,27 +13,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 
 public class UserController {
-
+    @Autowired
     private UserService userService;
-    private ElementAlredyExistsException exception;
 
-    public UserController(UserService userService, ElementAlredyExistsException exception) {
-        this.userService = userService;
-        this.exception = exception;
-    }
+
 
     @PostMapping("/addUser") // add new user and validation
-    public ResponseEntity<?> addUser(@RequestBody User user){
+    public ResponseEntity<?> addUser(@RequestBody User user) {
 
-        if(userService.findByEmail(user.getEmail()).isPresent() || userService.findByLastname(user.getLastname()).isPresent()){
+        if (userService.findByEmail(user.getEmail()).isPresent() || userService.findByLastname(user.getLastname()).isPresent()) {
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
-        if (user.getEmail()==null || user.getFirstname()==null || user.getLastname()==null){
-            return  new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if (user.getEmail() == null || user.getFirstname() == null || user.getLastname() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(userService.saveUser(user),HttpStatus.CREATED);
+        return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
     }
 
+    @GetMapping("/findUser/{userName}")
+    public ResponseEntity<?> getUserByName(@PathVariable("userName")String userName){
+
+    }
 
 
 }
