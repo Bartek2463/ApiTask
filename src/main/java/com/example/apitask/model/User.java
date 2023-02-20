aea9c138858264b2d7a9b8e81f72c7d0434ae595
Bartlomiej <szybiakbartlomiej@gmail.com>
2023-02-20 13:06:05 +0100

    Create User- fisrtname, lastname, email using adnotiations lombok for specific name.

--- src/main/java/com/example/apitask/model/User.java
+++ src/main/java/com/example/apitask/model/User.java
@@ -1,5 +1,32 @@
 package com.example.apitask.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "firstname", nullable = false)
+    @NonNull
+    private String firstname;
+
+    @Column(name = "lastname", nullable = false)
+    @NonNull
+    private String lastname;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
 
 }
